345b1712bcc7a5af498a92b0210f1815de1670bd
rmyscskn <77731166+rmyscskn@users.noreply.github.com>
2024-04-02 05:29:49 +0300

    Add files via upload

---
+++ constants/globalConstants.py
@@ -0,0 +1,8 @@
+BASE_URL = "https://www.saucedemo.com/"
+username_id = "user-name"
+password_id = "password"
+login_button_id = "login-button"
+errorMessage_xpath = "//*[@id='login_button_container']/div/form/div[3]/h3"
+errorMessage_text = "Epic sadface: Username and password do not match any user in this service"
+baslik_xpath = "//*[@id='header_container']/div[1]/div[2]/div"
+baslik_text = "Swag Labs"
